c4063db1e1c3604de78cc714a39fa4b363813c31
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-04-23 22:38:03 +0800

    Add a simple Assembly code.

---
+++ System_Software_Programming/01_First_Example/main.c
@@ -0,0 +1,26 @@
+﻿/**
+ * File Name: main.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build042322
+ *
+ * Date: 2022 / 04 / 23
+ *
+ * Description: 
+ * 1. 一個非常簡單的 *.c 檔案，並無太大的意義；主要是用來編譯成 Assembly。
+ *
+(*)?*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	signed int i = -1;
+
+	printf("signed int i = %d \n", i);
+
+	return 0;
+}
