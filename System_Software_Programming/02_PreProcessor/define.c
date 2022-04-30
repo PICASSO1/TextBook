52ab0d46e482e6166b949f5f5fa8db43d3b78df8
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-04-30 22:28:19 +0800

    Add two Assembly source code; but I don't know what are they doing ....

---
+++ System_Software_Programming/02_PreProcessor/define.c
@@ -0,0 +1,26 @@
+﻿/**
+ * File Name: define.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build042922
+ *
+ * Date: 2022 / 04 / 29
+ *
+ * Description: 
+ * 1. 這個範例用來呈現當前置處理器 (PreProcessor)，遇到巨集 (Macro)的時候是如何運作的。
+ *
+(*)?*/
+
+#include <stdio.h>
+
+#define MAX(A, B)	(A>B? A: B)
+
+int main(void)
+{
+	printf("MAX(4, 6): %d \n", MAX(4, 6));
+
+	return 0;
+}
---
+++ System_Software_Programming/02_PreProcessor/inline.c
@@ -0,0 +1,31 @@
+﻿/**
+ * File Name: inline.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build042922
+ *
+ * Date: 2022 / 04 / 29
+ *
+ * Description: 
+ * 1. 這個範例用來呈現當前置處理器 (PreProcessor)，遇到內嵌 (Inline)函式的時候是如何運作的。
+ *
+ * 2. "inline" 前面一定要加上 "static" ，要不然編譯一定不會過。
+ *
+(*)?*/
+
+#include <stdio.h>
+
+static inline int MIN(int X, int Y)
+{
+	return (X<Y? X: Y);
+}
+
+int main(void)
+{
+	printf("MIN(3, 9): %d \n", MIN(3, 9));
+
+	return 0;
+}
