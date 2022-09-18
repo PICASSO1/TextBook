f68b65395f348150168c83b57ce1ff1dcbb64009
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-09-18 22:45:50 +0800

    Add a 4th exercise for Chapter 4 ....

---
+++ TURBO_C_語言實務/CH04/4/main.c
@@ -0,0 +1,26 @@
+﻿/**
+ * File Name: main.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build091822
+ *
+ * Date: 2022 / 09 / 18
+ *
+ * Description: 將下列各數目以浮點數的科學表示法表示：
+ *
+(*)?*/
+
+#include <stdio.h>
+
+void main(void)
+{
+	printf("a). %E \n", (double)1245000);
+	printf("b). %E \n", (double)000456);
+	printf("c). %E \n", 35.95);
+	printf("d). %E \n", -99.99);
+
+	return;
+}
