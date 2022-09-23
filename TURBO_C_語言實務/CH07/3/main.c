f5853f177f5da37128ba564f9450b56331ef9c11
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-09-23 17:35:11 +0800

    Update ....

---
+++ TURBO_C_語言實務/CH07/3/main.c
@@ -0,0 +1,30 @@
+﻿/**
+ * File Name: main.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build092322
+ *
+ * Date: 2022 / 09 / 23
+ *
+ * Description: 用函式的寫法將鍵盤輸入的兩個數當作引述比較大小，並 return 值給主程式，印出數目較大者。
+ *
+(*)?*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+main(argc, argv, envp)
+int argc;
+char *argv[], **envp;
+{
+	if (argc != 3) {
+		fprintf(stderr, "ERROR!! Please enter two intergers!! \n");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
