a1dd0b2e3c4588d81c607230d6863376bba648b7
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-09-30 17:55:16 +0800

    Update ....

---
+++ TURBO_C_語言實務/CH10/7/main.c
@@ -0,0 +1,30 @@
+﻿/**
+ * File Name: main.c
+ *
+ * CopyLeft (C) 2022, Picasso's Fantasy Notepad. All rights reserved.
+ *
+ * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
+ *
+ * Version: 1.0.0.build093022
+ *
+ * Date: 2022 / 09 / 30
+ *
+ * Description: 利用１０－２節的串列結構：
+ *              struct {
+ *                  char info[10];
+ *                  struct node *next;
+ *              } node;
+ *              並定義一個陣列 struct node a[50], *b; 寫一對函式 in(char i); 與 out(char i); 。
+ *              in(i) 會由 ａ[] 找一個空ｎｏｄｅ，把字串ｉ存入ｎｏｄｅ，然後把該ｎｏｄｅ有序地加入到一個串列ｂ裡 (最初為空串列)，
+ *              out(i) 會把字串ｉ由串列中取出，且由串列中刪去該ｎｏｄｅ，把ｎｏｄｅ清空交還給 ａ[]。
+ *
+(*)?*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    return 0;
+}
