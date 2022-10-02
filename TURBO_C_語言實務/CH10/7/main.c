ae1e9d4eed95a3bacfadd2475f6afa2612c46e18
PICASSO1 <neogeo.snk@msa.hinet.net>
2022-10-02 23:05:28 +0800

    MDFK!!

--- TURBO_C_語言實務/CH10/7/main.c
+++ TURBO_C_語言實務/CH10/7/main.c
@@ -5,9 +5,9 @@
  *
  * Author: Pablo Picasso G. (PabloPicasso.G@gmail.com)
  *
- * Version: 1.0.0.build093022
+ * Version: 1.0.0.build100222
  *
- * Date: 2022 / 09 / 30
+ * Date: 2022 / 10 / 02
  *
  * Description: 利用１０－２節的串列結構：
  *              struct {
@@ -21,10 +21,81 @@
 (*)?*/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct node {
+	char info[10];
+	struct node *next;
+} a[50], *b = (struct node *)NULL;
+
+const static char *data[10] = {"Peter", "Mary", "John", "May", "Picasso", "Eric", "Sam", "Apple", "Lucia", "Rose"};
+
+void in(const char *);
+void out(const char *);
 
 int main(void)
 {
+	size_t size = 10U, idx = 0U;
+	// Initialize the global array .... 
+	memset(a, 0x00, sizeof(struct node) * 10);
+	for (idx = 0; idx < 2; idx++) {
+		strncpy(a[idx].info, data[idx], strlen(data[idx]));
+		in(data[idx]);
+	}
 
+	// display the content of a[];
+/*	for (idx = 0; idx < 10; idx++)
+		fprintf(stdout, "a[%zu].info: %s (%zu) \n", idx, a[idx].info, strlen(a[idx].info));
+*/
+	out(data[0]);
+	out(data[1]);
 
     return 0;
+}
+
+void in(const char *NAME)
+{
+	struct node *current = (struct node *)NULL;
+	struct node *last = (struct node *)NULL;
+
+	current = (struct node *)malloc(sizeof(struct node));
+	memset(current->info, '\0', sizeof(char) * 10);
+	strncpy(current->info, NAME, strlen(NAME));
+	current->next = (struct node *)NULL;
+	// for testing!!
+	fprintf(stdout, "%s: current->info: %s \n", __FUNCTION__, current->info);
+	if (b == (struct node *)NULL) {
+		b = current;
+		return;
+	}
+	fprintf(stdout, "@@@@\n");
+	last = b;
+	while (last != (struct node *)NULL) {
+		fprintf(stdout, "while: %s \n", last->info);
+		last = last->next;
+	}
+	fprintf(stdout, "QQQQ\n");
+	last = current;
+	fprintf(stdout, "1234\n");
+	return;
+}
+
+void out(const char *NAME)
+{
+	struct node *current = b;
+	struct node *head = (struct node *)NULL;
+	
+	while (current != (struct node *)NULL) {
+		printf("%s: current->info: %s \n", __FUNCTION__, current->info);
+		current = current->next;
+		head = current;
+//		printf("%s: current->next->info: %s \n", __FUNCTION__, current->next->info);
+		free((void *)head);
+	//	strncpy(current->info, NAME, strlen(NAME));
+	}
+
+	
+
+	return;
 }
