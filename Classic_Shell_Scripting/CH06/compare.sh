6d121cfc3398b0a5f1577f07cb8e22bac0e4a876
PICASSO1 <neogeo.snk@msa.hinet.net>
2023-08-02 18:28:12 +0800

    Update compare.sh....

--- Classic_Shell_Scripting/CH06/compare.sh
+++ Classic_Shell_Scripting/CH06/compare.sh
@@ -15,3 +15,20 @@
 
 #!/bin/bash - 
 
+VAR="ABCDEABCDEABCDEVWXYZVWXYZVWXYZ"
+
+printf "\${VAR}:       %s (%d) \n" ${VAR} ${#VAR}
+
+# VAR 變數當中，前面有符合 "ABCDE" 的部份移除
+echo "\${VAR#ABCDE}: "${VAR#ABCDE}
+
+# VAR 變數當中，前面有符合 "A*E" 的部份全部移除
+echo "\${VAR##A*E}:  "${VAR##A*E}
+
+# VAR 變數當中，後面有符合 "VWXYZ" 的部份移除
+echo "\${VAR%VWXYZ}: "${VAR%VWXYZ}
+
+# VAR 變數當中，後面有符合 "V*Z" 的部份全部移除
+echo "\${VAR%%V*Z}:  "${VAR%%V*Z}
+
+exit 5
